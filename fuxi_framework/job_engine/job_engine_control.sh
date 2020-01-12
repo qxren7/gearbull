ccc49bae24c57c1981295099aca41577da02b6e8
qxren7 <>
2020-01-12 19:56:54 +0800

    remove old codes

--- fuxi_framework/job_engine/job_engine_control.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ps aux | grep job_scheduler | grep -v grep | awk '{print $2}' | xargs -i kill -9 {}
-ps aux | grep job_monitor | grep -v grep | awk '{print $2}' | xargs -i kill -9 {}
-
-python2.7 job_scheduler.py > ../log/scheduler_daemon.log 2>&1 &
-python2.7 job_monitor.py > ../log/monitor_daemon.log 2>&1 &
